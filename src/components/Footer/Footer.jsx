761cb609b5f1fc48fb9d9c235467a44b25217436
Denys <tymchenko.denys@gmail.com>
2023-03-14 01:40:26 +0200

    Footer created

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import { Typography } from "@mui/material";
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer>
+      <Typography variant="body1">Created by: Denys Tymchenko</Typography>
+
+      <div className="socials">
+        <a href="https://github.com/DenysTymchenko">
+          <GitHubIcon />
+        </a>
+
+        <a href="https://t.me/DenysTymchenko">
+          <TelegramIcon />
+        </a>
+
+        <a href="mailto:tymchenko.denys@gamil.com">
+          <EmailIcon />
+        </a>
+
+        <a href="https://www.linkedin.com/in/denys-tymchenko-0031b1251/">
+          <LinkedInIcon />
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
