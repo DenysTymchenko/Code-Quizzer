92e23897ab377147f283c3121e7dc8b3ebb48578
Denys <tymchenko.denys@gmail.com>
2023-03-14 12:17:06 +0200

    Header updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AlarmOnIcon from '@mui/icons-material/AlarmOn';
+import HomeIcon from '@mui/icons-material/Home';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Typography } from "@mui/material";
 import './Header.css'
@@ -7,7 +8,12 @@ import './Header.css'
 function Header() {
   return(
     <header>
-      <div className="wrapper">
+      <div className="page-name">
+        <HomeIcon />
+        <Typography variant="h5">Home</Typography>
+      </div>
+
+      <div className="logo">
         <AlarmOnIcon />
         <Typography variant="h5">Quiz App</Typography>
       </div>
