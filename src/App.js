1a72f09b56cc95900a09906417d5706a7c50e617
Denys <tymchenko.denys@gmail.com>
2023-03-14 11:58:55 +0200

    Main page added to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      <Main />
     </div>
   );
 }
