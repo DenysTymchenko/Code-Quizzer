e71f75065dd24009bc4f70a1246f757cc7504472
Denys <tymchenko.denys@gmail.com>
2023-03-14 18:28:06 +0200

    Main page example created and added to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import Main from "./pages/Main";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,17 +1,32 @@
 import React from "react";
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
+import Typography from '@mui/material/Typography';
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
+import videoBg from '../assets/videoBg.mp4';
 import "./Main.css"
 
 function Main() {
   return (
-    <>
-      <Header />
-      <main>
+    <main>
+      <section className="greetings">
+        <video src={videoBg} autoPlay loop muted></video>
 
-      </main>
-      <Footer />
-    </>
+        <div className="wrapper">
+          <Typography variant="h1" gutterBottom>
+            Welcome to Quizz App!
+          </Typography>
+
+          <Typography variant="h2" gutterBottom>
+            Place where you can check your programing skills.
+          </Typography>
+
+          <a href="#quizzes">
+            <KeyboardDoubleArrowDownIcon style={{width: '50px', height: '50px'}}/>
+          </a>
+        </div>
+      </section>
+
+      <section id="quizzes"></section>
+    </main>
   )
 }
 
