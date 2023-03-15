02f9d86c95c74c71c1d7c28b61d5439f44137813
Denys <tymchenko.denys@gmail.com>
2023-03-15 12:09:30 +0200

    Main.jsx styled

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -6,16 +6,38 @@ import "./Main.css"
 import axios from "axios";
 import QuizCard from "../components/QuizCard/QuizCard";
 
+const h1Styles = {
+  textAlign: 'center',
+  "@media (max-width:600px)": {
+    fontSize: "4rem",
+    fontWeight: 600,
+  },
+}
+
+const h2Styles = {
+  textAlign: 'center',
+  "@media (max-width:600px)": {
+    fontSize: "2rem",
+  },
+}
+
+const h3Styles = {
+  textAlign: 'center',
+  marginBottom: 5,
+  "@media (max-width:600px)": {
+    fontSize: "2.5rem",
+  },
+}
+
 function Main() {
   let [quizzes, setQuizzes] = useState([]);
 
   useEffect(() => {
     (async () => {
-      const { data } = await axios.get("https://63ef5f59271439b7fe6d10b5.mockapi.io/quizzes");
+      const {data} = await axios.get("https://63ef5f59271439b7fe6d10b5.mockapi.io/quizzes");
       setQuizzes(data);
     })();
   }, []);
-
 
   return (
     <main>
@@ -23,11 +45,11 @@ function Main() {
         <video src={videoBg} autoPlay loop muted></video>
 
         <div className="wrapper">
-          <Typography variant="h1" gutterBottom>
+          <Typography variant="h1" gutterBottom sx={h1Styles}>
             Welcome to Quizz App!
           </Typography>
 
-          <Typography variant="h2" gutterBottom>
+          <Typography variant="h2" gutterBottom sx={h2Styles}>
             Place where you can check your programing skills.
           </Typography>
 
@@ -38,14 +60,18 @@ function Main() {
       </section>
 
       <section id="quizzes">
-        {quizzes.map(quiz => (
-          <QuizCard
-            key={quiz.id}
-            img={quiz.img}
-            title={quiz.title}
-            description={quiz.description}
-          />
-        ))}
+        <Typography variant="h3" sx={h3Styles}>
+          Best way to start
+        </Typography>
+
+        <div className="wrapper">
+          {quizzes.map(quiz => (
+            <QuizCard
+              key={quiz.id}
+              quiz={quiz}
+            />
+          ))}
+        </div>
       </section>
     </main>
   )
