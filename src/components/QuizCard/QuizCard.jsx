7226020a0713513a09e0f63bd74e02dbd7216651
Denys <tymchenko.denys@gmail.com>
2023-03-15 12:08:29 +0200

    QuizModal created and rendered in QuizCard

--- src/components/QuizCard/QuizCard.jsx
+++ src/components/QuizCard/QuizCard.jsx
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from '@mui/material/Card';
+import Alert from '@mui/material/Alert';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import QuizModal from "../QuizModal/QuizModal";
 
-function QuizCard({img, title, description}) {
-  console.log(img, title, description);
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  zIndex: '9999',
+}
+
+function QuizCard({key, quiz}) {
+
+  const [isStarted, setStart] = useState(false);
+  const handleStart = () => setStart(!isStarted); //temporary for open/close alert
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <Card sx={{maxWidth: 345, height: 'fit-content'}}>
+    <Card key={key} sx={{maxWidth: 345, height: 'fit-content'}}>
       <CardMedia
         sx={{height: 300}}
-        image={img}
-        title={title}
+        image={quiz.img}
+        title={quiz.title}
       />
 
       <CardContent
@@ -23,10 +40,10 @@ function QuizCard({img, title, description}) {
           textAlign: 'center',
         }}>
         <Typography gutterBottom variant="h5" component="div">
-          {title}
+          {quiz.title}
         </Typography>
         <Typography variant="body2">
-          {description}
+          {quiz.description}
         </Typography>
       </CardContent>
 
@@ -37,8 +54,37 @@ function QuizCard({img, title, description}) {
           backgroundColor: '#252422',
         }}
       >
-        <Button size="small">Start</Button>
-        <Button size="small">Learn More</Button>
+        <Button
+          size="small"
+          variant="outlined"
+          sx={{
+            color: 'white',
+            borderColor: '#F7EC59',
+            "&:hover": {
+              borderColor: '#FEC601',
+            }
+          }}
+          onClick={handleStart}
+        >
+          Start
+        </Button>
+        {isStarted && <Alert severity="success" sx={style}>Quiz started (Temporary realisation)</Alert>}
+
+        <Button
+          size="small"
+          variant="outlined"
+          sx={{
+            color: 'white',
+            borderColor: '#F7EC59',
+            "&:hover": {
+              borderColor: '#FEC601',
+            }
+          }}
+          onClick={handleOpen}
+        >
+          Learn More
+        </Button>
+        <QuizModal open={open} handleClose={handleClose} quiz={quiz} handleStart={handleStart} />
       </CardActions>
     </Card>
   );
---
+++ src/components/QuizModal/QuizModal.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import './QuizModal.css'
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 300,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  textAlign: 'center',
+  bgcolor: '#252422',
+  color: 'white',
+  border: '2px solid #000',
+  boxShadow: 24,
+  paddingBottom: '10px',
+};
+
+function QuizModal({ open, handleClose, quiz, handleStart }) {
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <img src={quiz.img} alt={quiz.title}/>
+
+        <div className="info">
+          <Typography gutterBottom variant="h5" component="div">
+            {quiz.title}
+          </Typography>
+
+          <Typography variant="body2">
+            {quiz.description}
+          </Typography>
+        </div>
+
+        <Button
+          size="small"
+          variant="outlined"
+          sx={{
+            color: 'white',
+            borderColor: '#F7EC59',
+            "&:hover": {
+              borderColor: '#FEC601',
+            }
+          }}
+          onClick={handleStart}
+        >
+          Start
+        </Button>
+      </Box>
+    </Modal>
+  );
+}
+
+export default QuizModal;
