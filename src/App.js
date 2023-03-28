dd630626897d15686c1ed33e70d8a00f99fd7b99
Denys <tymchenko.denys@gmail.com>
2023-03-29 00:24:23 +0300

    FavoritesPage component + Route path created

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Main from './pages/Main/Main';
-import NotFound from './pages/NotFound/NotFound';
 import Quizzes from './pages/QuizzesPage/QuizzesPage';
+import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route index element={<Main />} />
           <Route path='/quizzes' element={<Quizzes />} />
+          <Route path='/favorites' element={<FavoritesPage />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FavoritePage() {
+  return (
+    <>fav</>
+  );
+}
+
+export default FavoritePage;
